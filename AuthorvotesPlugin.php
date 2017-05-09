c2af403fe8baa9d8e5311cb8cb66e6d22bb89184
Jerry Chai <jerry@gooddogdesign.com>
2017-05-09 09:09:17 -0700

    add documentation url

--- AuthorvotesPlugin.php
+++ AuthorvotesPlugin.php
@@ -2,33 +2,38 @@
 namespace Craft;
 
 /**
- * Entry Count Plugin
- */
+* Entry Count Plugin
+*/
 class AuthorvotesPlugin extends BasePlugin
 {
-    public function getName()
-    {
-        return Craft::t('Author Votes');
-    }
+  public function getName()
+  {
+    return Craft::t('Author Votes');
+  }
 
-    public function getVersion()
-    {
-        return '1.0.0';
-    }
+  public function getVersion()
+  {
+    return '1.0.0';
+  }
 
-    public function getDeveloper()
-    {
-        return 'Jerry Chai';
-    }
+  public function getDeveloper()
+  {
+    return 'Jerry Chai';
+  }
 
-    public function getDeveloperUrl()
-    {
-        return 'http://jerrychai.us';
-    }
+  public function getDeveloperUrl()
+  {
+    return 'http://jerrychai.us';
+  }
 
-    public function hasCpSection()
-    {
-        return false;
-    }
+  public function getDocumentationUrl()
+  {
+    return 'https://github.com/jchai002/authorvotes';
+  }
+
+  public function hasCpSection()
+  {
+    return false;
+  }
 
 }
